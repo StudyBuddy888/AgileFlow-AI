9c8bd26d2b3c4871a9983fce0c137a995cc6bea9
Padmacharan M <lorecafe888@gmail.com>
2025-08-27 17:59:27 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+# Read requirements from requirements.txt
+with open('requirements.txt') as f:
+    requirements = [line.strip() for line in f if line.strip() and not line.startswith('#')]
+
+setup(
+    name="Reporter Agent",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=requirements
+)
+
+#pip install -e .
